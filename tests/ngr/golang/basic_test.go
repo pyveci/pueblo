2be993c610dc1789547f080ce7e8c4910b52b092
Andreas Motl <andreas.motl@panodata.org>
2023-11-01 01:34:16 +0100

    ngr: Add runner for Golang projects

---
+++ tests/ngr/golang/basic_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+func TestFooBar(t *testing.T) {
+    assert.Equal(t, 42, 42)
+}
---
+++ tests/ngr/golang/main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+    println("Hallo, Räuber Hotzenplotz.")
+}
