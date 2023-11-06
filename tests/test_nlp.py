f5f0ecdeb4cef11d85970f6bf740d26d0a1875b4
Andreas Motl <andreas.motl@panodata.org>
2023-11-06 17:10:55 +0100

    Chore: Code formatting

--- tests/test_nlp.py
+++ tests/test_nlp.py
@@ -1,7 +1,6 @@
-from pueblo.nlp.resource import CachedWebResource
-
-
 def test_cached_web_resource():
+    from pueblo.nlp.resource import CachedWebResource
+
     url = "https://github.com/langchain-ai/langchain/raw/v0.0.325/docs/docs/modules/state_of_the_union.txt"
     docs = CachedWebResource(url).langchain_documents(chunk_size=1000, chunk_overlap=0)
     assert len(docs) == 42
