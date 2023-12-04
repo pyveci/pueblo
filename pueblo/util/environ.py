0bfb40f6fdf46e72a03cc5e072f381530dc1e56b
Andreas Motl <andreas.motl@panodata.org>
2023-12-04 05:22:39 +0100

    Chore: Modularize `getenvpass` function

--- pueblo/util/environ.py
+++ pueblo/util/environ.py
@@ -1,10 +1,18 @@
 import getpass
 import os
+import typing as t
 
 from dotenv import find_dotenv, load_dotenv
 
 
-def getenvpass(env_var: str, prompt: str, skip_pytest_notebook: bool = True) -> None:
+def init_dotenv():
+    """
+    Find `.env` file and load environment variables.
+    """
+    load_dotenv(find_dotenv())
+
+
+def getenvpass(env_var: str, prompt: t.Union[str, None] = None, skip_pytest_notebook: bool = True) -> None:
     """
     Read variable from environment or `.env` file.
     If it is not defined, prompt interactively.
@@ -12,11 +20,12 @@ def getenvpass(env_var: str, prompt: str, skip_pytest_notebook: bool = True) -> 
     FIXME: Needs a patch to make it work with `pytest-notebook`,
            see https://github.com/chrisjsewell/pytest-notebook/issues/43.
     """
-    load_dotenv(find_dotenv())
+
+    init_dotenv()
     if env_var not in os.environ:
         if "PYTEST_CURRENT_TEST" in os.environ and skip_pytest_notebook:
             import pytest
 
             pytest.exit(f"{env_var} not given [skip-notebook]")
-        else:
+        elif prompt:
             os.environ[env_var] = getpass.getpass(prompt)
