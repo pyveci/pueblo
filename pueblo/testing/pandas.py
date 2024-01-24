b6c6289cc4d806b75bf9dba2c54580973827d2d3
Andreas Motl <andreas.motl@panodata.org>
2024-01-25 00:03:38 +0100

    Testing: Add `pueblo.testing.pandas.makeMixedDataFrame`
    It was removed from `pandas._testing` on behalf of pandas 2.2.0.

--- pueblo/testing/pandas.py
+++ pueblo/testing/pandas.py
@@ -1,3 +1,5 @@
+import typing as t
+
 import numpy as np
 import pandas as pd
 
@@ -18,3 +20,20 @@ def makeTimeDataFrame(nper=None, freq="B") -> pd.DataFrame:
         columns=pd.Index(list("ABCD"), dtype=object),
         index=pd.date_range("2000-01-01", periods=nper, freq=freq),
     )
+
+
+def getMixedTypeDict() -> t.Tuple[pd.Index, t.Dict[str, t.Any]]:
+    index = pd.Index(["a", "b", "c", "d", "e"])
+
+    data = {
+        "A": [0.0, 1.0, 2.0, 3.0, 4.0],
+        "B": [0.0, 1.0, 0.0, 1.0, 0.0],
+        "C": ["foo1", "foo2", "foo3", "foo4", "foo5"],
+        "D": pd.bdate_range("1/1/2009", periods=5),
+    }
+
+    return index, data
+
+
+def makeMixedDataFrame() -> pd.DataFrame:
+    return pd.DataFrame(getMixedTypeDict()[1])
